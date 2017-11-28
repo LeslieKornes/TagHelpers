95ae4d84c0a81a3fd2626c7ebfe6ae73bf0d8212
Leslie Kornes <Lesliekornes@gmail.com>
2017-11-28 16:33:00 -0500

    added ButtonTagHelper.cs in Infrastructure/TagHelpers folder

--- src/Cities/Startup.cs
+++ src/Cities/Startup.cs
@@ -1,9 +1,6 @@
 ﻿using Cities.Models;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Cities
 {
