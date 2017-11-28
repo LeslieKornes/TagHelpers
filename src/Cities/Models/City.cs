297fe1b61ab3f6218ab79d4215433795aa320dfb
Leslie Kornes <Lesliekornes@gmail.com>
2017-11-28 16:05:49 -0500

    added index.cshtml in Views/Home

--- src/Cities/Models/City.cs
+++ src/Cities/Models/City.cs
@@ -7,5 +7,8 @@ namespace Cities.Models
 {
     public class City
     {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int? Population { get; set; }
     }
 }
--- src/Cities/Models/Repository.cs
+++ src/Cities/Models/Repository.cs
@@ -5,7 +5,27 @@ using System.Threading.Tasks;
 
 namespace Cities.Models
 {
-    public class Repository
+    public interface IRepository
     {
+        IEnumerable<City> Cities { get; }
+        void AddCity(City newCity);
+    }
+
+    public class MemoryRepository : IRepository
+    {
+        private List<City> cities = new List<City>
+        {
+            new City { Name = "London", Country = "UK", Population = 8539000 },
+            new City { Name = "New York", Country = "USA", Population = 8406000 },
+            new City { Name = "San Jose", Country = "USA", Population = 998537 },
+            new City { Name = "Paris", Country = "France", Population = 8539000 }
+        };
+
+        public IEnumerable<City> Cities => cities;
+
+        public void AddCity(City newCity)
+        {
+            cities.Add(newCity);
+        }
     }
 }
