efb40505401eaca11b83f591789d08d1e7685e6e
Leslie Kornes <Lesliekornes@gmail.com>
2017-11-29 10:56:07 -0500

    applied different HtmlTargetElement attribute to ButtonTagHelper to broaden the scope and then performed the most balanced approach

--- src/Cities/Infrastructure/TagHelpers/ButtonTagHelper.cs
+++ src/Cities/Infrastructure/TagHelpers/ButtonTagHelper.cs
@@ -6,8 +6,19 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Cities.Infrastructure.TagHelpers
 {
-    //applying restrictions with this tag helper attribute so as to narrow the scope of the tag helper
+    //applying restrictions with this tag helper attribute so as to NARROW the scope of the tag helper
+    //this attribute stopped the tag helper from blocking the btn-primary style applied to the reset btn
+    //[HtmlTargetElement("button", Attributes = "bs-button-color", ParentTag = "form")]
+
+
+    //applying restrictions with this tag helper attribute so as to WIDEN the scope of the tag helper
+    //this attribute will enable the a tag to also be able to use the tag helper, not just buttons
+    //[HtmlTargetElement(Attributes = "bs-button-color", ParentTag = "form")]
+
+
+    //The most balanced approach:
     [HtmlTargetElement("button", Attributes = "bs-button-color", ParentTag = "form")]
+    [HtmlTargetElement("a", Attributes = "bs-button-color", ParentTag = "form")]
     public class ButtonTagHelper : TagHelper
     {
         public string BsButtonColor { get; set; }
